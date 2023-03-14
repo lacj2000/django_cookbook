88bfc37ebbe37eb897b68fa83ea7c2c60ce85ba7
lacj2000 <luizjuniordeveloper@gmail.com>
2023-03-13 21:21:29 -0300

    create ourview

--- myapplication/urls.py
+++ myapplication/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('hello', views.hello, name= 'hello'),
     path('hi', views.hi, name= 'hi'),
+
     path('ourview', views.OurView.as_view(), name="our_view"),
 ]
 
